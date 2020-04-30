c2f641e259d36f726a03cbc93cf690cf61b9dbbf
Sue <suyu@create80.com>
2020-04-30 17:22:11 +0800

    Initial commit

---
+++ backbone/ssd.py
@@ -0,0 +1,10 @@
+from __future__ import division
+import numpy as np
+from tensorflow.keras.models import Model
+from keras.layers import Input, Lambda, Conv2D, MaxPooling2D, BatchNormalization, ELU, Reshape, Concatenate, Activation
+from keras.regularizers import l2
+import keras.backend as K
+
+from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_layers.keras_layer_DecodeDetectionsFast import DecodeDetectionsFast
